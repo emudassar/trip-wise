21edb813b3815ce28eebf5c503df07b643da0e3f
emudassar <contacmudassar@gmail.com>
2025-06-11 08:12:05 +0500

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3A8A", 
+        secondary: "#F97316",
+        cream: "#FFF7ED", 
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
